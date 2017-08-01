4fac32b8531dc1a3e65b748bf5ebe2d5cc94d6d6
MrElektronix <milovanpelt@gmail.com>
2017-08-01 14:26:53 +0200

    update

---
+++ Assets/MyEnum.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyEnum : MonoBehaviour {
+
+	public enum WeaponTypes
+	{
+		Sword,
+		Mace, 
+		Pistol
+	}
+	[SerializeField]
+	private WeaponTypes weapontype;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			switch (weapontype) {
+			case WeaponTypes.Mace:
+				MaceAttack ();
+				break;
+			case WeaponTypes.Sword:
+				SwordAttack ();
+				break;
+			case WeaponTypes.Pistol:
+				PistolAttack ();
+				break;
+			default:
+				break;
+			}
+		}
+	}
+
+	void MaceAttack() {
+		Debug.Log ("Mace op je kop");
+	}
+
+	void SwordAttack() {
+		Debug.Log ("Sword op je kop");
+	}
+
+	void PistolAttack() {
+		Debug.Log ("Kogel door je hoofd");
+	}
+}
---
+++ Assets/RandomColor.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomColor : MonoBehaviour {
+	private int random;
+	Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			GenerateColor ();
+		}
+	}
+
+	void GenerateColor() {
+		random = Random.Range (0,6);
+		Color newColor = new Color ();
+		switch (random) {
+		case 0:
+			newColor = Color.black;
+			break;
+		case 1:
+			newColor = Color.red;
+			break;
+		case 2:
+			newColor = Color.blue;
+			break;
+		case 3:
+			newColor = Color.yellow;
+			break;
+		case 4:
+			newColor = Color.green;
+			break;
+		case 5:
+			newColor = Color.grey;
+			break;
+		case 6:
+			newColor = Color.magenta;
+			break;
+		default:
+			newColor = Color.white;
+			break;
+		}
+		rend.material.color = newColor;
+	}
+}
